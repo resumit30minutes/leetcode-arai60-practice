299ae2b5a29f01d498e08d345e07ee8fb59f0a11
Hiroki-Sato9 <resumi30minutes@gmail.com>
2025-09-16 23:18:57 +0900

    step3まで

---
+++ leetcode/remove_duplicates_from_sorted_list/step2.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+
+        while curr is not None and curr.next is not None:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        
+        return head
